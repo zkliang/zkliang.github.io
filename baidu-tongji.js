b33db2a8e2a214e62e9decfff48783a9c4de3ecb
zkliang747-cloud <zkliang747@gmail.com>
2026-07-23 10:48:55 +0800

    feat: 接入百度统计（首页+shop.html，CSP 放行 hm.baidu.com）

---
+++ baidu-tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?ba294b48047db333bf359f2b35ce7b6f";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
